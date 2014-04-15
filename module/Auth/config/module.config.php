04b7a492e0f7df37ba17117fea1dee6aff3c40f3
Napster2k3 <c.greselle33@gmail.com>
2014-04-15 21:45:26 +0200

    DoctrineExtendedUser

--- module/Auth/config/module.config.php
+++ module/Auth/config/module.config.php
@@ -1,11 +1,45 @@
 <?php
 
-include_once(__DIR__ . '/../src/ZfcUser/Controller/UserController.php');
-
 return array(
     'controllers' => array(
         'invokables' => array(
             'Auth\Controller\User' => 'Auth\Controller\UserController',
+        ),
+    ),
+
+    'doctrine' => array(
+        'driver' => array(
+            // overriding zfc-user-doctrine-orm's config
+            'zfcuser_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'paths' => __DIR__ . '/../src/Auth/Entity',
+            ),
+
+            'orm_default' => array(
+                'drivers' => array(
+                    'Auth\Entity' => 'zfcuser_entity',
+                ),
+            ),
+        ),
+    ),
+
+    'zfcuser' => array(
+        // telling ZfcUser to use our own class
+        'user_entity_class'       => 'Auth\Entity\User',
+        // telling ZfcUserDoctrineORM to skip the entities it defines
+        'enable_default_entities' => false,
+    ),
+
+    'bjyauthorize' => array(
+        // Using the authentication identity provider, which basically reads the roles from the auth service's identity
+        'identity_provider' => 'BjyAuthorize\Provider\Identity\AuthenticationIdentityProvider',
+
+        'role_providers'        => array(
+            // using an object repository (entity repository) to load all roles into our ACL
+            'BjyAuthorize\Provider\Role\ObjectRepositoryProvider' => array(
+                'object_manager'    => 'doctrine.entitymanager.orm_default',
+                'role_entity_class' => 'Auth\Entity\Role',
+            ),
         ),
     ),
 
